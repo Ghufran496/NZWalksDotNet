087dad64bc63122d0ffd291ca2b7f3a5d690dd3c
Ghufran Ullah <92268694+Ghufran496@users.noreply.github.com>
2024-03-29 12:40:13 +0500

    Section-2_Completed

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        //shortcut for constructor ctor double tab
+
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+        //we will create DBset property for each of our Model property
+
+        public DbSet<Difficulty> Difficulties  { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+        //all of these three DB context properties represents collection in our database that we will be creating.
+    }
+}
